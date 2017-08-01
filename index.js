18e12a1282caa248016b0a7cdab5b326cbb510e2
LiubomyrPenkov <lubchikpenkov@gmail.com>
2017-08-01 15:54:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ io.on('connection', function (socket) {
             id: socket.id
         }
         users.push(user);
-
+        
+        //display online users
         io.emit('online users', users);
+        
+        //broadcast a message when user connects
         socket.broadcast.emit('display message', "<strong>" + user.username + "</strong>" + " connected");
 
         socket.on('submit message', function (msg) {
+            //check if it is the private message
             for (var i = 0; i < users.length; i++) {
                 if (!msg.toLowerCase().indexOf( "(" + users[i].username.toLowerCase() +")")) {
                     msg = msg.slice(users[i].username.toLowerCase().length+2)
@@ -25,8 +29,10 @@ io.on('connection', function (socket) {
                     return;
                 }
             }
+            //if not -> send to all users
             socket.broadcast.emit('display message', "<strong>" + user.username + "</strong>" + ": " + msg);
         })
+        //broadcast a message who is typing. Delete it in ${time} ms after last keyup
         socket.on('type', (time) => {
             clearTimeout(timer);
             socket.broadcast.emit("typing", "<strong>" + user.username + "</strong>");
@@ -37,6 +43,7 @@ io.on('connection', function (socket) {
 
 
         socket.on('disconnect', function () {
+            //broadcast a message when user disconnect + delete from online users list
             socket.broadcast.emit('display message', "<strong>" + user.username + "</strong>" + " disconnected");
             users.splice(users.indexOf(user.username), 1);
             io.emit('online users', users);
